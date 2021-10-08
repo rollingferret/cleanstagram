9c11dee5e135ff4b699b0370500dd892b6fbbe59
Kenneth Lee <kenneth.lee.developer@gmail.com>
2021-10-08 14:36:02 -0700

    added validator to check password for special symbols

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -20,9 +20,22 @@ def username_exists(form, field):
         raise ValidationError('Username is already in use.')
 
 
+def check_password(form, field):
+    # Checking if password is longer than or equal to 8 chars
+    # Checking if password contains !@#$%^&*(),./?
+    special_sym = '!@#$%^&*(),./?'
+    password = field.data
+
+    # if none of the chars in password is in special_sym
+    if not any(char for char in password if char in special_sym):
+        raise ValidationError(
+            'Password must contain at least one of these following characters: !@#$%^&*(),./?')
+
+
 class SignUpForm(FlaskForm):
     username = StringField(
         'username', validators=[DataRequired(), username_exists])
     email = StringField('email', validators=[
                         DataRequired(), user_exists, Email()])
-    password = StringField('password', validators=[DataRequired()])
+    password = StringField('password', validators=[
+                           DataRequired(), check_password])
