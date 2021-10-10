e4a6731f7fbdbed126623968e94446c9fa4ad012
Kenneth Lee <kenneth.lee.developer@gmail.com>
2021-10-10 13:08:36 -0700

    added images table and relationship between images and users

---
+++ app/models/images.py
@@ -0,0 +1,30 @@
+from .db import db
+from datetime import datetime
+
+
+class Image(db.Model):
+    __tablename__ = 'images'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    image_url = db.Column(db.String(255), nullable=False)
+    caption = db.Column(db.Text, nullable=True)
+    likes_count = db.Column(db.Integer, nullable=False, default=0)
+    comments_count = db.Column(db.Integer, nullable=False, default=0)
+    created_at = db.Column(db.DateTime, nullable=False)
+    updated_at = db.Column(db.DateTime, nullable=False)
+
+    # relationships
+    user = db.relationship('User', back_populates='images', uselist=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'image_url': self.image_url,
+            'caption': self.caption,
+            'likes_count': self.likes_count,
+            'comments_count': self.comments_count,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
+        }
