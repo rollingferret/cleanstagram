d5df2402b4c05cf3f929aca7db0ba5f73b9a7436
Kenneth Lee <kenneth.lee.developer@gmail.com>
2021-10-12 10:26:00 -0700

    backend route for following and unfollowing users works

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, jsonify
-from flask_login import login_required
-from app.models import User
+from flask_login import login_required, current_user
+from flask_migrate import current
+from app.api.auth_routes import login
+from app.models import User, db
 
 user_routes = Blueprint('users', __name__)
 
@@ -10,6 +12,53 @@ user_routes = Blueprint('users', __name__)
 def users():
     users = User.query.all()
     return {'users': [user.to_dict() for user in users]}
+
+
+@user_routes.route('/<int:id>/follow', methods=['GET'])
+@login_required
+def follow_user(id):
+    '''
+    user that is currently logged in can follow the user whose id is in the params.
+    '''
+
+    curr_user = User.query.get(current_user.get_id())
+    if not curr_user:
+        return {'errors': 'Current user does not exist.'}
+
+    user_to_follow = User.query.get(id)
+    if not user_to_follow:
+        return {'errors': 'User to be followed does not exist.'}
+
+    curr_user.followers.append(user_to_follow)
+
+    db.session.add(curr_user)
+    db.session.add(user_to_follow)
+    db.session.commit()
+
+    return 'something'
+
+
+@user_routes.route('/<int:id>/follow', methods=['DELETE'])
+@login_required
+def unfollow_user(id):
+    '''
+    user that is currently logged in can unfollow the user whose id is in the params
+    '''
+    curr_user = User.query.get(current_user.get_id())
+    if not curr_user:
+        return {'errors': 'Current user does not exist.'}
+
+    user_to_unfollow = User.query.get(id)
+    if not user_to_unfollow:
+        return {'errors': 'User to be followed does not exist.'}
+
+    curr_user.followers.remove(user_to_unfollow)
+
+    db.session.add(curr_user)
+    db.session.add(user_to_unfollow)
+    db.session.commit()
+
+    return 'something'
 
 
 @user_routes.route('/<int:id>')
