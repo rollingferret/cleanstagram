563f192b6255d656d1409e6218f165b9876b56fd
Kenneth Lee <kenneth.lee.developer@gmail.com>
2021-10-12 14:14:14 -0700

    styling nav bar, moved logout button into navbar component

--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -1,48 +1,56 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
-import LogoutButton from "./auth/LogoutButton";
+import { useDispatch } from "react-redux";
+import { logout } from "../../store/session";
+
+import LogoutButton from "../auth/LogoutButton";
+import styles from "./NavBar.module.css";
 
 const NavBar = () => {
   const currentUser = useSelector((state) => state.session.user);
+  const dispatch = useDispatch();
+  const onLogout = async (e) => {
+    await dispatch(logout());
+  };
 
   const user_buttons = () => {
     if (currentUser) {
       return (
-        <>
+        <div className={styles.logged_in_buttons}>
           <li>
             <NavLink to="/users" exact={true} activeClassName="active">
               Users
             </NavLink>
           </li>
           <li>
-            <LogoutButton />
+            <button onClick={onLogout}>Logout</button>
           </li>
-        </>
+        </div>
       );
     } else {
       return (
-        <>
+        <div className={styles.logged_out_buttons}>
           <li>
             <NavLink to="/sign-up" exact={true} activeClassName="active">
               Sign Up
             </NavLink>
           </li>
-        </>
+        </div>
       );
     }
   };
 
   return (
-    <nav>
-      <ul>
+    <nav className={styles.navbar}>
+      <div className={styles.home_button}>
         <li>
           <NavLink to="/" exact={true} activeClassName="active">
             Home
           </NavLink>
         </li>
-        {user_buttons()}
-      </ul>
+      </div>
+      {user_buttons()}
     </nav>
   );
 };
