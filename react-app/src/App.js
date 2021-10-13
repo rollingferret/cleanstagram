ddaad6722b51285a8675428491766b4b447d4934
Taylor Barnabic <tbarnabic1@gmail.com>
2021-10-13 18:48:17 -0400

    Files for Feed Component created

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,63 +1,65 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
-import SignUpForm from "./components/auth/SignUpForm";
-import NavBar from "./components/NavBar";
-import ProtectedRoute from "./components/auth/ProtectedRoute";
-import UsersList from "./components/UsersList";
-import User from "./components/User";
-import ImageForm from "./components/ImageForm";
-import ProfilePage from "./components/ProfilePage";
-import LoginFormPage from "./components/LoginFormPage";
-import { authenticate } from "./store/session";
-import ImagePage from "./components/ImagePage";
+import SignUpForm from './components/auth/SignUpForm';
+import NavBar from './components/NavBar';
+import ProtectedRoute from './components/auth/ProtectedRoute';
+import UsersList from './components/UsersList';
+import User from './components/User';
+import ImageForm from './components/ImageForm';
+import ProfilePage from './components/ProfilePage';
+import LoginFormPage from './components/LoginFormPage';
+import { authenticate } from './store/session';
+import ImagePage from './components/ImagePage';
+import Feed from './components/Feed';
 
 function App() {
-  const [loaded, setLoaded] = useState(false);
-  const dispatch = useDispatch();
+	const [loaded, setLoaded] = useState(false);
+	const dispatch = useDispatch();
 
-  useEffect(() => {
-    (async () => {
-      await dispatch(authenticate());
-      setLoaded(true);
-    })();
-  }, [dispatch]);
+	useEffect(() => {
+		(async () => {
+			await dispatch(authenticate());
+			setLoaded(true);
+		})();
+	}, [dispatch]);
 
-  if (!loaded) {
-    return null;
-  }
+	if (!loaded) {
+		return null;
+	}
 
-  return (
-    <BrowserRouter>
-      <NavBar />
-      <Switch>
-        <ProtectedRoute path="/add_image" exact={true}>
-          <ImageForm />
-        </ProtectedRoute>
-        <Route path="/sign-up" exact={true}>
-          <SignUpForm />
-        </Route>
-        <ProtectedRoute path="/users" exact={true}>
-          <UsersList />
-        </ProtectedRoute>
-        <Route path="/users/:userId" exact={true}>
-          <ProfilePage />
-        </Route>
-        <Route path="/" exact>
-          {/* login form page */}
-          <LoginFormPage />
-        </Route>
-        <ProtectedRoute path="/home" exact={true}>
-          {/* home page if logged in show photo feed */}
-          <h1>My Home Page</h1>
-        </ProtectedRoute>
-        <ProtectedRoute path="/images/:imageId">
-          <ImagePage />
-        </ProtectedRoute>
-      </Switch>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<NavBar />
+			<Switch>
+				<ProtectedRoute path="/add_image" exact={true}>
+					<ImageForm />
+				</ProtectedRoute>
+				<Route path="/sign-up" exact={true}>
+					<SignUpForm />
+				</Route>
+				<ProtectedRoute path="/users" exact={true}>
+					<UsersList />
+				</ProtectedRoute>
+				<Route path="/users/:userId" exact={true}>
+					<ProfilePage />
+				</Route>
+				<Route path="/" exact>
+					{/* login form page */}
+					<LoginFormPage />
+				</Route>
+				<ProtectedRoute path="/home" exact={true}>
+					{/* home page if logged in show photo feed */}
+					<h1>My Home Page</h1>
+					<Feed />
+				</ProtectedRoute>
+				<ProtectedRoute path="/images/:imageId">
+					<ImagePage />
+				</ProtectedRoute>
+			</Switch>
+		</BrowserRouter>
+	);
 }
 
 export default App;
---
+++ react-app/src/components/Feed/index.js
@@ -0,0 +1,12 @@
+import css from './Feed.module.css';
+
+function Feed() {
+	return (
+		<div>
+			<span>BOOP</span>
+			<div>BOP</div>
+		</div>
+	);
+}
+
+export default Feed;
