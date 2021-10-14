d251160ae076992e38af91e8694ccd2567f751c9
Kenneth Lee <kenneth.lee.developer@gmail.com>
2021-10-13 21:22:58 -0700

    fixed comments showing on all posts instead of just one post

--- react-app/src/components/CommentDisplayComponent/index.js
+++ react-app/src/components/CommentDisplayComponent/index.js
@@ -1,72 +1,42 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
-import { getCommentByIdThunk } from '../../store/comments';
-import { NavLink } from 'react-router-dom';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { getCommentByIdThunk } from "../../store/comments";
+import { NavLink } from "react-router-dom";
 
+function GetAllCommentsForSinglePhoto({ imageId }) {
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-function GetAllCommentsForSinglePhoto() {
-
-    const dispatch = useDispatch();
-    const history = useHistory();
-
-    // const { productId } = useParams();
-    // console.log('teeeeeeeeeeeeeeeeeeeeeeest', history);
-    // const product = useSelector(state => state?.product?.product)
-    
-    const commentList = useSelector(state => {
-        return state.comments;
-    });
-
-    // console.log(commentList, '9999999999999999999999999999999999999999999')
-
-    // const stateList = useSelector(state => {
-    //     return state;
-    // });
-
-    const {pathname} = history.location
-    const singlePhotoId = pathname.split("/")[2]
-
-    // console.log(singlePhotoId, '99999999999999999999999999999999999999999')
-
-    
-    useEffect(() => {
-        dispatch(getCommentByIdThunk(singlePhotoId))
-    },[dispatch])
-    
-    // useEffect(() => {
-    //     dispatch();
-    // }, [dispatch]);
-
-    // console.log(commentList.map((comment => {return comment.id})))
-
-    // for (const property in commentList) {
-    //     console.log(`${property}: ${commentList[property].id}`);
-    //   }
-
-    // console.log(Object.entries(commentList), '222222222222222222222222222222')
-
-    const commentsSection = Object.values(commentList)?.map(comment => (
-        <div key={comment.id} className='single-comment'>
-        <div>{comment.id}</div>
-        <div>{comment.content}</div>
-        <div>{comment.updated_at}</div>
-      </div>
-      ))
-
-    // console.log(stateList, "33333333333333333333333333333333333333333")
-
-
-
-    if (!commentList) {
-        return <div>things went wrong</div>;
-    } else {
-    return (
-        <>
-            {commentsSection}
-        </>
+  const commentList = useSelector((state) => {
+    return state.comments;
+  });
+  let currentComments;
+  if (commentList) {
+    currentComments = Object.values(commentList).filter(
+      (comment) => comment.image_id === +imageId
     );
-    }
-};
+  }
+  console.log("this is commentList", commentList);
+  const { pathname } = history.location;
+  const singlePhotoId = pathname.split("/")[2];
+
+  useEffect(() => {
+    dispatch(getCommentByIdThunk(singlePhotoId));
+  }, [dispatch]);
+  const commentsSection = Object.values(currentComments)?.map((comment) => (
+    <div key={comment.id} className="single-comment">
+      <div>{comment.id}</div>
+      <div>{comment.content}</div>
+      <div>{comment.updated_at}</div>
+    </div>
+  ));
+
+  if (!commentList) {
+    return <div>things went wrong</div>;
+  } else {
+    return <>{commentsSection}</>;
+  }
+}
 
 export default GetAllCommentsForSinglePhoto;
--- react-app/src/components/ImagePage/index.js
+++ react-app/src/components/ImagePage/index.js
@@ -1,141 +1,137 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useParams, useHistory, Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams, useHistory, Link } from "react-router-dom";
 
-import LikeButton from '../LikeButton/index';
-import { getImageById, deleteImage, updateCaption } from '../../store/images';
+import LikeButton from "../LikeButton/index";
+import { getImageById, deleteImage, updateCaption } from "../../store/images";
 
-import imageForm from './ImageForm.module.css';
-import GetAllCommentsForSinglePhoto from '../CommentDisplayComponent';
-import NewCommentForm from '../NewCommentForm';
-
+import imageForm from "./ImageForm.module.css";
+import GetAllCommentsForSinglePhoto from "../CommentDisplayComponent";
+import NewCommentForm from "../NewCommentForm";
 
 function ImagePage() {
-	const sessionUser = useSelector((state) => state.session.user);
-	const image = useSelector((state) => state.images.currentImage);
-	const history = useHistory();
-	const dispatch = useDispatch();
-	const { imageId } = useParams();
+  const sessionUser = useSelector((state) => state.session.user);
+  const image = useSelector((state) => state.images.currentImage);
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const { imageId } = useParams();
 
-	const [editButtons, setEditButtons] = useState(false);
-	const [edit, setEdit] = useState(false);
-	const [caption, setCaption] = useState(image?.caption);
+  const [editButtons, setEditButtons] = useState(false);
+  const [edit, setEdit] = useState(false);
+  const [caption, setCaption] = useState(image?.caption);
 
-	const newDate = image?.created_at.split(' ');
+  const newDate = image?.created_at.split(" ");
 
-	useEffect(() => {
-		dispatch(getImageById(imageId));
-	}, [dispatch, imageId]);
+  useEffect(() => {
+    dispatch(getImageById(imageId));
+  }, [dispatch, imageId]);
 
-	useEffect(() => {
-		if (sessionUser.id === image?.user_id) {
-			setEditButtons(true);
-		}
-	}, [sessionUser.id, image?.user_id]);
+  useEffect(() => {
+    if (sessionUser.id === image?.user_id) {
+      setEditButtons(true);
+    }
+  }, [sessionUser.id, image?.user_id]);
 
-	const updateSubmit = async (e) => {
-		e.preventDefault();
+  const updateSubmit = async (e) => {
+    e.preventDefault();
 
-		const payload = {
-			id: image.id,
-			caption,
-		};
+    const payload = {
+      id: image.id,
+      caption,
+    };
 
-		let res = await dispatch(updateCaption(payload));
-		if (res.ok) {
-			setEdit(false);
-		}
-	};
+    let res = await dispatch(updateCaption(payload));
+    if (res.ok) {
+      setEdit(false);
+    }
+  };
 
-	const onDelete = async () => {
-		const toDelete = image.id;
+  const onDelete = async () => {
+    const toDelete = image.id;
 
-		let res = await dispatch(deleteImage(toDelete));
-		if (res.ok) {
-			history.push(`/home`);
-		}
-	};
+    let res = await dispatch(deleteImage(toDelete));
+    if (res.ok) {
+      history.push(`/home`);
+    }
+  };
 
-	const onEdit = () => {
-		setEdit(!edit);
-	};
+  const onEdit = () => {
+    setEdit(!edit);
+  };
 
-	let editDelBtns;
-	if (editButtons) {
-		editDelBtns = (
-			<div>
-				<button className={imageForm.btns} onClick={onEdit}>
-					Edit
-				</button>
-				<button className={imageForm.btns} onClick={onDelete}>
-					Delete
-				</button>
-			</div>
-		);
-	}
+  let editDelBtns;
+  if (editButtons) {
+    editDelBtns = (
+      <div>
+        <button className={imageForm.btns} onClick={onEdit}>
+          Edit
+        </button>
+        <button className={imageForm.btns} onClick={onDelete}>
+          Delete
+        </button>
+      </div>
+    );
+  }
 
-	let editForm;
-	if (edit) {
-		editForm = (
-			<form onSubmit={updateSubmit}>
-				<input
-					type="text"
-					value={caption}
-					onChange={(e) => setCaption(e.target.value)}
-				/>
-				<button type="submit">Update</button>
-				<button onClick={() => setEdit(false)}>Cancel</button>
-			</form>
-		);
-	} else {
-		editForm = <p>{image?.caption}</p>;
-	}
+  let editForm;
+  if (edit) {
+    editForm = (
+      <form onSubmit={updateSubmit}>
+        <input
+          type="text"
+          value={caption}
+          onChange={(e) => setCaption(e.target.value)}
+        />
+        <button type="submit">Update</button>
+        <button onClick={() => setEdit(false)}>Cancel</button>
+      </form>
+    );
+  } else {
+    editForm = <p>{image?.caption}</p>;
+  }
 
-	if (!image) return null;
+  if (!image) return null;
 
-	return (
-		<div className={imageForm.outercontainer}>
-			<div className={imageForm.innercontainer}>
-				<div className={imageForm.imgcontainer}>
-					<img
-						className={imageForm.img}
-						src={image.image_url}
-						alt={image.caption}
-					/>
-				</div>
-				<div className={imageForm.rightcontainer}>
-					<div className={imageForm.usercontainer}>
-						<Link
-							to={`/users/${image.user_id}`}
-							className={imageForm.username}
-						>
-							{image.user.username}
-						</Link>
-						{editForm}
-						{editDelBtns}
-					</div>
-					<div className={imageForm.commentcontainer}>
-					<GetAllCommentsForSinglePhoto />
-					<NewCommentForm />
-					</div>
-					<div className={imageForm.likecommentcontainer}>
-						<div>
-							<span className={imageForm.likecomment}>
-								{image.likes_count} likes
-							</span>
-							<span className={imageForm.likecomment}>
-								{image.comments_count} comments
-							</span>
-						</div>
-						<p>
-							{newDate[2]} {newDate[1]}, {newDate[3]}
-						</p>
-					</div>
-				</div>
-			</div>
-			<LikeButton id={image.id} />
-		</div>
-	);
+  return (
+    <div className={imageForm.outercontainer}>
+      <div className={imageForm.innercontainer}>
+        <div className={imageForm.imgcontainer}>
+          <img
+            className={imageForm.img}
+            src={image.image_url}
+            alt={image.caption}
+          />
+        </div>
+        <div className={imageForm.rightcontainer}>
+          <div className={imageForm.usercontainer}>
+            <Link to={`/users/${image.user_id}`} className={imageForm.username}>
+              {image.user.username}
+            </Link>
+            {editForm}
+            {editDelBtns}
+          </div>
+          <div className={imageForm.commentcontainer}>
+            <GetAllCommentsForSinglePhoto imageId={imageId} />
+            <NewCommentForm />
+          </div>
+          <div className={imageForm.likecommentcontainer}>
+            <div>
+              <span className={imageForm.likecomment}>
+                {image.likes_count} likes
+              </span>
+              <span className={imageForm.likecomment}>
+                {image.comments_count} comments
+              </span>
+            </div>
+            <p>
+              {newDate[2]} {newDate[1]}, {newDate[3]}
+            </p>
+          </div>
+        </div>
+      </div>
+      <LikeButton id={image.id} />
+    </div>
+  );
 }
 
 export default ImagePage;
