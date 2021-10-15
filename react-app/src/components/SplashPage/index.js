8ccc7b0aa6b7ce02d9d274048e706a910a36f74b
Peter Joh <johpeter@gmail.com>
2021-10-15 14:58:34 -0600

    isnt a group project without losing work

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -2,15 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import splash_page from "./splash_page.css";
 import LoginFormPage from "../LoginFormPage";
-
+import phoneborder from './images/phoneborder.png'
 
 function SplashPage() {
 
     return (
         <>
         <div className="splashpage_container_main">
-        <div className="splashpage_container_main_left">
-            <img src="/phoneborder.png" />
+        <div className="splashpage_container_main_left" style={{backgroundImage: `url(${phoneborder})`}}>
         </div>
         <div className="splashpage_container_main_right">
             <h1>We've hit the right side!</h1>
