2a11387e6fe88be38f3c0e14ab6b2de2632e68c3
Kenneth Lee <kenneth.lee.developer@gmail.com>
2021-10-15 16:50:41 -0700

    changed login form css to css module

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Redirect } from "react-router-dom";
 import { login } from "../../store/session";
-import loginformcss from './loginformcss.css'
+import styles from "./LoginForm.module.css";
 
 const LoginForm = () => {
   const [errors, setErrors] = useState([]);
@@ -32,39 +32,39 @@ const LoginForm = () => {
   }
 
   return (
-    <div className='authloginform_outter_container'>
-    <form onSubmit={onLogin}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
-      </div>
-      <div className='authloginform_inner_container'>
-      <div>
-        <label htmlFor="login_param">tester</label>
-        <input
-          name="login_param"
-          type="text"
-          placeholder="login_param"
-          value={login_param}
-          onChange={updateLoginParam}
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <input
-          name="password"
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={updatePassword}
-        />
-        </div>
+    <div className={styles.authloginform_outter_container}>
+      <form onSubmit={onLogin}>
         <div>
-        <button type="submit">Login</button>
+          {errors.map((error, ind) => (
+            <div key={ind}>{error}</div>
+          ))}
         </div>
-      </div>
-    </form>
+        <div className={styles.authloginform_inner_container}>
+          <div>
+            <label htmlFor="login_param">tester</label>
+            <input
+              name="login_param"
+              type="text"
+              placeholder="login_param"
+              value={login_param}
+              onChange={updateLoginParam}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input
+              name="password"
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={updatePassword}
+            />
+          </div>
+          <div>
+            <button type="submit">Login</button>
+          </div>
+        </div>
+      </form>
     </div>
   );
 };
