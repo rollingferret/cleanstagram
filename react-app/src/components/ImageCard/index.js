762aabf1058bc91cf40f4741954e7110868a47fb
Taylor Barnabic <tbarnabic1@gmail.com>
2021-10-14 20:23:01 -0400

    More CSS

--- react-app/src/components/ImageCard/index.js
+++ react-app/src/components/ImageCard/index.js
@@ -5,13 +5,30 @@ function ImageCard(...props) {
 		<div className={css['card-container']}>
 			<div className={css['user-container']}>{props.userName}</div>
 			<div className={css['image-container']}>Image Here</div>
-			<div className={css['btn-container']}>Like Comment Buttons Div</div>
-			<div className={css['like-display-container']}>Like Count</div>
+			<div className={css['btn-container']}>
+				<btn className={css['interaction-btn']} id={css.red}></btn>
+				<btn className={css['interaction-btn']} id={css.blue}></btn>
+			</div>
+			<div className={css['like-display-container']}>
+				Liked by Bob and 414 others
+			</div>
 			<div className={css['caption-container']}>{props.caption}</div>
 			<div className={css['date-comment-container']}>
-				First Three Comments Div/Date
+				Posted X Hours Ago
 			</div>
-			<div className={css['add-comment-container']}>Add a Comment</div>
+			<div className={css['add-comment-container']}>
+				<form>
+					{/* <textarea className={css['comment-input']} /> */}
+					<input
+						type="text"
+						placeholder="Add a comment..."
+						className={css['comment-input']}
+					/>
+					<button type="submit" className={css['comment-btn']}>
+						Post
+					</button>
+				</form>
+			</div>
 		</div>
 	);
 }
