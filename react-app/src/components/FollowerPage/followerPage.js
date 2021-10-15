4ea424af2c938bead15cf0201df5321ab3f13856
Kenneth Lee <kenneth.lee.developer@gmail.com>
2021-10-15 09:08:31 -0700

    follower modal now shows usernames

--- react-app/src/components/FollowerPage/followerPage.js
+++ react-app/src/components/FollowerPage/followerPage.js
@@ -1,7 +1,19 @@
+import { useSelector } from "react-redux";
+import styles from "./followerPageModal.module.css";
+
 function FollowerPage({ user }) {
+  const allUsers = useSelector((state) => state.users);
+  let followers;
+
+  if (Object.keys(allUsers).length > 0) {
+    followers = user.followers.map((followerId) => allUsers[followerId]);
+  }
+
   return (
-    <div>
-      <p>FOLLOWer PAGE!</p>
+    <div className={styles.modal_body}>
+      {followers?.map((follower) => (
+        <h1>{follower.username}</h1>
+      ))}
     </div>
   );
 }
