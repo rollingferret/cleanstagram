9859ca024af504cb9ce4ad83075762cef5bbbeee
William <williamhjang@gmail.com>
2021-10-16 17:05:04 -0400

    created result card + styled results page

--- react-app/src/components/Results/index.js
+++ react-app/src/components/Results/index.js
@@ -3,6 +3,10 @@ import { useDispatch, useSelector } from "react-redux";
 import { useParams, Link } from "react-router-dom";
 import { getResults } from "../../store/results";
 
+import ResultCard from "./ResultCard";
+
+import styles from "./Results.module.css"
+
 function Result() {
     const dispatch = useDispatch();
     const userQuery = useSelector((state) => state.results);
@@ -19,15 +23,13 @@ function Result() {
 
     return (
         <>
-            <h1>We've hit the results page!</h1>
-            <ul>
+            <ul className={styles.list}>
                 {" "}
-                Search Results:
+                <p className={styles.results_title
+                }>Search Results:</p>
                 {filterArr.map((user) => (
                     <li>
-                        <Link to={`/users/${user.id}`} key={user.id}>
-                            {user.username}
-                        </Link>
+                        <ResultCard user={user} />
                     </li>
                 ))}
             </ul>
