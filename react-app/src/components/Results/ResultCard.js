16ba96dd9affdef59fd9fc01e954ce8818ec2952
Kenneth Lee <kenneth.lee.developer@gmail.com>
2021-10-17 13:50:12 -0700

    fixed being able to like image twice from frontend

--- react-app/src/components/Results/ResultCard.js
+++ react-app/src/components/Results/ResultCard.js
@@ -1,26 +1,24 @@
 import React from "react";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-import styles from './Results.module.css'
+import styles from "./Results.module.css";
 
 function ResultCard({ user }) {
-
-    console.log(user)
-    return (
-        <div className={styles.card_container}>
-            <img alt="user_profile_pic"
-                src={user.profile_url}
-                className={styles.profile_picture}
-            />
-            <div className={styles.user_info}>
-                <Link to={`/users/${user.id}`}
-                className={styles.user_name}>
-                    {user.username}
-                </Link>
-                <p className={styles.user_bio}>{user.bio}</p>
-            </div>
-        </div>
-    )
+  return (
+    <div className={styles.card_container}>
+      <img
+        alt="user_profile_pic"
+        src={user.profile_url}
+        className={styles.profile_picture}
+      />
+      <div className={styles.user_info}>
+        <Link to={`/users/${user.id}`} className={styles.user_name}>
+          {user.username}
+        </Link>
+        <p className={styles.user_bio}>{user.bio}</p>
+      </div>
+    </div>
+  );
 }
 
-export default ResultCard
+export default ResultCard;
