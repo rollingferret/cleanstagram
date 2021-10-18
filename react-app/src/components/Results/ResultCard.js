248e63f403d87b3b59a361c75e5e8314eb353f27
Peter Joh <johpeter@gmail.com>
2021-10-18 02:14:49 -0600

    added buffer div to results page

--- react-app/src/components/Results/ResultCard.js
+++ react-app/src/components/Results/ResultCard.js
@@ -6,11 +6,13 @@ import styles from "./Results.module.css";
 function ResultCard({ user }) {
   return (
     <div className={styles.card_container}>
+      <div className={styles.profile_picture_buffer}>
       <div
         alt="user_profile_pic"
         style={{ backgroundImage: `url(${user.profile_url})` }}
         className={styles.profile_picture}
       />
+      </div>
       <div className={styles.user_info}>
         <Link to={`/users/${user.id}`} className={styles.user_name}>
           {user.username}
