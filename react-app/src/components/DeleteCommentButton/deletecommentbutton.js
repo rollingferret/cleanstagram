6b94ba6de8cb2a6a6dbd4780623a6705a828be54
Peter Joh <johpeter@gmail.com>
2021-10-17 19:09:52 -0600

    delete modal css

--- react-app/src/components/DeleteCommentButton/deletecommentbutton.js
+++ react-app/src/components/DeleteCommentButton/deletecommentbutton.js
@@ -1,5 +1,7 @@
 import { useDispatch } from "react-redux";
 import { delCommentThunk } from "../../store/comments";
+import deletecommentbutton from "./deletecommentbutton.css"
+
 
 function DeleteCommentButton(props) {
   const dispatch = useDispatch();
@@ -11,8 +13,9 @@ function DeleteCommentButton(props) {
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <button type="submit">Delete!</button>
+      <form onSubmit={handleSubmit} className="deletecommentbuttoncss">
+        <div className="deletebuttoninnercss">Are you sure you want to delete?</div>
+        <button type="submit" className='deletebuttonsubmitcss'>Delete!</button>
       </form>
     </>
   );
