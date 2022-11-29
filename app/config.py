f6fc9ff7c8c892b6cfa213ff9b9963c62b1ab923
Kenneth Lee <kenneth.lee.developer@gmail.com>
2022-11-29 12:47:44 -0800

    Update documentation

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # SQLAlchemy 1.4 no longer supports url strings that start with 'postgres'
-    # (only 'postgresql') but heroku's postgres add-on automatically sets the
+    # (only 'postgresql') but Render's postgres add-on automatically sets the
     # url in the hidden config vars to start with postgres.
     # so the connection uri must be updated here
     SQLALCHEMY_DATABASE_URI = os.environ.get(
