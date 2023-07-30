17c7272cac7bd3d1e72a67a1d9299486224d6a76
PJ <johpeter@gmail.com>
2023-07-30 13:54:41 -0600

    Update index.js

--- react-app/src/components/LikeButton/index.js
+++ react-app/src/components/LikeButton/index.js
@@ -54,20 +54,75 @@ import css from "./LikeComponent.module.css";
 
 // export default LikeButton;
 
+// function LikeButton({ id }) {
+//   const dispatch = useDispatch();
+//   const image = useSelector((state) => state.images[id]);
+
+//   useEffect(() => {
+//     dispatch(checkLikeStatus(id));
+//   }, [dispatch, id]);
+
+//   const handleDislike = async (e) => {
+//     dispatch(dislikeImage(id));
+//   };
+
+//   const handleLike = async (e) => {
+//     dispatch(likeImage(id));
+//   };
+
+//   return (
+//     <div>
+//       {image && image.isLiked ? (
+//         <button
+//           className={`fas fa-heart ${css.redlikebutton}`}
+//           onClick={handleDislike}
+//         ></button>
+//       ) : (
+//         <button
+//           className={`far fa-heart ${css.whitelikebutton}`}
+//           onClick={handleLike}
+//         ></button>
+//       )}
+//     </div>
+//   );
+// }
+
+// export default LikeButton;
+
 function LikeButton({ id }) {
   const dispatch = useDispatch();
   const image = useSelector((state) => state.images[id]);
+
+  const [isProcessing, setIsProcessing] = useState(false); // Add a state to track the button's processing state
 
   useEffect(() => {
     dispatch(checkLikeStatus(id));
   }, [dispatch, id]);
 
   const handleDislike = async (e) => {
-    dispatch(dislikeImage(id));
+    if (!isProcessing) {
+      setIsProcessing(true); // Disable the button to prevent spamming
+
+      try {
+        await dispatch(dislikeImage(id));
+        setIsProcessing(false); // Re-enable the button after the action is completed
+      } catch (error) {
+        setIsProcessing(false); // Re-enable the button if an error occurs
+      }
+    }
   };
 
   const handleLike = async (e) => {
-    dispatch(likeImage(id));
+    if (!isProcessing) {
+      setIsProcessing(true); // Disable the button to prevent spamming
+
+      try {
+        await dispatch(likeImage(id));
+        setIsProcessing(false); // Re-enable the button after the action is completed
+      } catch (error) {
+        setIsProcessing(false); // Re-enable the button if an error occurs
+      }
+    }
   };
 
   return (
@@ -76,11 +131,13 @@ function LikeButton({ id }) {
         <button
           className={`fas fa-heart ${css.redlikebutton}`}
           onClick={handleDislike}
+          disabled={isProcessing} // Disable the button while processing
         ></button>
       ) : (
         <button
           className={`far fa-heart ${css.whitelikebutton}`}
           onClick={handleLike}
+          disabled={isProcessing} // Disable the button while processing
         ></button>
       )}
     </div>
