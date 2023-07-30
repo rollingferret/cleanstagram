b636fcd0c449c52c856092b581609062a96d7f6e
PJ <johpeter@gmail.com>
2023-07-30 13:49:24 -0600

    Update index.js

--- react-app/src/components/LikeButton/index.js
+++ react-app/src/components/LikeButton/index.js
@@ -4,40 +4,75 @@ import { checkLikeStatus, dislikeImage, likeImage } from "../../store/images";
 
 import css from "./LikeComponent.module.css";
 
+// function LikeButton({ id }) {
+//   const dispatch = useDispatch();
+
+//   const [liked, setLiked] = useState(false);
+
+//   const image = useSelector((state) => state.images[id]);
+//   let isLiked;
+//   if (image && Object.keys(image).length > 0) {
+//     isLiked = image.isLiked;
+
+//     if (isLiked !== liked) {
+//       setLiked(isLiked);
+//     }
+//   }
+
+//   useEffect(() => {
+//     (async () => {
+//       dispatch(checkLikeStatus(id));
+//     })();
+//   }, [dispatch, id]);
+
+//   const handleDislike = async (e) => {
+//     dispatch(dislikeImage(id));
+//     image.isLiked = false;
+//   };
+
+//   const handleLike = async (e) => {
+//     dispatch(likeImage(id));
+//     image.isLiked = true;
+//   };
+
+//   return (
+//     <div>
+//       {image && image?.isLiked ? (
+//         <button
+//           className={`fas fa-heart ${css.redlikebutton}`}
+//           onClick={handleDislike}
+//         ></button>
+//       ) : (
+//         <button
+//           className={`far fa-heart ${css.whitelikebutton}`}
+//           onClick={handleLike}
+//         ></button>
+//       )}
+//     </div>
+//   );
+// }
+
+// export default LikeButton;
+
 function LikeButton({ id }) {
   const dispatch = useDispatch();
-
-  const [liked, setLiked] = useState(false);
-
   const image = useSelector((state) => state.images[id]);
-  let isLiked;
-  if (image && Object.keys(image).length > 0) {
-    isLiked = image.isLiked;
-
-    if (isLiked !== liked) {
-      setLiked(isLiked);
-    }
-  }
 
   useEffect(() => {
-    (async () => {
-      dispatch(checkLikeStatus(id));
-    })();
+    dispatch(checkLikeStatus(id));
   }, [dispatch, id]);
 
   const handleDislike = async (e) => {
     dispatch(dislikeImage(id));
-    image.isLiked = false;
   };
 
   const handleLike = async (e) => {
     dispatch(likeImage(id));
-    image.isLiked = true;
   };
 
   return (
     <div>
-      {image && image?.isLiked ? (
+      {image && image.isLiked ? (
         <button
           className={`fas fa-heart ${css.redlikebutton}`}
           onClick={handleDislike}
